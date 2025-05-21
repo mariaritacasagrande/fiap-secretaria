20cf3ac21564dea27c19501a0e70ec98ba245a88
Maria Rita Casagrande <mariarita@riseup.net>
2025-05-21 19:44:42 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,18 +1,21 @@
 <?php
 
 /**
-* Sistema desenvolvido por Maria Rita Casagrande
-* © 2025 Maria Rita Casagrande - Todos os direitos reservados
-* Repositório: https://github.com/mariaritacasagrande/fiap-secretaria
-*/
-class Database {
+ * Sistema desenvolvido por Maria Rita Casagrande
+ * © 2025 Maria Rita Casagrande - Todos os direitos reservados
+ * Repositório: https://github.com/mariaritacasagrande/fiap-secretaria
+ */
+
+class Database
+{
     private $host = "localhost";
     private $db_name = "fiap_secretaria";
     private $username = "root";
     private $password = "";
     public $conn;
 
-    public function connect() {
+    public function connect()
+    {
         $this->conn = null;
         try {
             $this->conn = new PDO(
@@ -21,7 +24,7 @@ class Database {
                 $this->password
             );
             $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
+        } catch (PDOException $exception) {
             echo "Erro de conexão: " . $exception->getMessage();
         }
         return $this->conn;
